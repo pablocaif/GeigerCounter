bfc57e97be29b149775548452728451b790d5c8a
Pablo Caif <pablocaif@gmail.com>
2018-02-10 20:06:13 +1100

    Initial Commit

---
+++ GeigerCounter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeigerCounter
+//
+//  Created by Pablo Caif on 10/2/18.
+//  Copyright © 2018 Pablo Caif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
